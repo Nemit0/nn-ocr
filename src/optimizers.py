a28659f903e73e0893b631b07da17859ed061301
Nemit <123555510+Nemit0@users.noreply.github.com>
2024-06-24 21:59:11 +0900

    Update

--- src/optimizers.py
+++ src/optimizers.py
@@ -4,20 +4,24 @@ def gradiant_descent(weights, biases, gradients, learning_rate):
     raise NotImplementedError
 
 class optimizers(object):
-    @abstractmethod 
-    def update(self, weights, gradients):
+    @abstractmethod
+    def update(self, weights, biases, gradients) -> tuple:
         pass
 
 class Naive(optimizers):
-    def __init__
+    def __init__(self, learning_rate: float):
+        self.learning_rate = learning_rate
+
+    def update(self, weights, biases, gradients):
+        return weights - self.learning_rate * gradients, biases - self.learning_rate * gradients
 
 class SGD(optimizers):
     def __init__(self, learning_rate: float):
         self.learning_rate = learning_rate
 
-    def update(self, weights, gradients):
-        return weights - self.learning_rate * gradients
-    
+    def update(self, weights, biases, gradients):
+        return weights - self.learning_rate * gradients, biases - self.learning_rate * gradients
+
 class Momentum(optimizers):
     def __init__(self, learning_rate: float, beta: float):
         self.learning_rate = learning_rate
